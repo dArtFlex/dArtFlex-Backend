a34f250b590aa4152eed18f2d50e61e5f227c8bc
harry.liu <harry.liu@ideasoft.io>
2021-05-20 02:36:27 -0400

    update schema for lazy minting

--- Server/migrations/20210519161721_lazyminting.js
+++ Server/migrations/20210519161721_lazyminting.js
@@ -3,12 +3,12 @@ exports.up = function(knex, Promise) {
     return knex.schema
     .createTable('lazymint', function(t) {
         t.increments('id').primary().notNullable()
-        t.string('contract').notNullable().unique()
+        t.string('contract').notNullable()
         t.integer('tokenId').notNullable().unique()
         t.string('uri').notNullable()
-        t.string('creator')
+        t.string('creator').notNullable()
         t.string('royalty')
-        t.string('signatures')
+        t.string('signatures').notNullable()
         t.timestamp('created_at').defaultTo(knex.fn.now())
         t.timestamp('updated_at').defaultTo(knex.fn.now())
     })
