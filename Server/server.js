d5866eac8a348e68a02b92bd52c161ce28dbc62b
tatyana <tatyana@loanbase.com>
2021-09-30 15:10:41 +0300

    RAD-709: fix buy now marketplace

--- Server/server.js
+++ Server/server.js
@@ -6,14 +6,7 @@ const cron = require('node-cron');
 const port = 8888
 const app = express()
 const server = require('http').createServer(app);
-const io = require('socket.io')(server, {
-    cors: {
-        origin : "*",
-        // methods: ["GET", "POST"],
-        // credentials: true
-    }
-
-});
+const io = require('socket.io')(server);
 
 const swaggerUI = require('swagger-ui-express');
 const swaggerJsDoc = require('swagger-jsdoc');
@@ -89,7 +82,7 @@ app.use(function(request, response, next){
 });
 
 app.use(express.static('public'));
-// app.use(bodyParser.json());
+app.use(bodyParser.json());
 app.use(bodyParser.json({limit: '500mb'}));
 app.use(bodyParser.urlencoded({limit: '500mb', extended: true}));
 app.use(cors());
