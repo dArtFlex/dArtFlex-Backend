9b0a20ea9081444765bb1f2cbf851434b9c58c44
Roman Tiutiun <roman.t@ideasoft.io>
2021-10-13 17:37:53 +0300

    prod

--- Server/src/controller/SubscribeController.js
+++ Server/src/controller/SubscribeController.js
@@ -2,7 +2,7 @@ const Web3 = require("web3");
 const secrets= require('../../secrets.js')
 const knex = require('knex')(secrets.database)
 
-const web3 = new Web3(new Web3.providers.WebsocketProvider('wss://rinkeby.infura.io/ws/v3/2de4d25aeea745b181468b898cf4e899'));
+const web3 = new Web3(new Web3.providers.WebsocketProvider('wss://mainnet.infura.io/ws/v3/2de4d25aeea745b181468b898cf4e899'));
 
 async function watchEtherTransfers() {
 	const topic = web3.utils.keccak256('Transfer(address,address,uint256)');
